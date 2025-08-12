580df61351554aaf65bd51612aeb2f745282446b
oscarsidsa81 <ovilches@sid-sa.com>
2025-08-12 13:24:43 +0200

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'sid_sale_route_id_forms_and_lines',
+    'version': '1.0',
+    'category': 'Sales',
+    'license': 'AGPL-3',
+    'summary': 'Campos en vista tree y form para visualizar las líneas de factura por línea en form y line',
+    'description': 'Campos en vista tree y form para visualizar las líneas de factura por línea en form y line',
+    'author': 'oscarsidsa81',
+    'depends': ['base','sale_management','account_accountant'],
+    'data': [
+        'views/sid_sale_route_id_forms_and_lines.xml'
+    ],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+}
