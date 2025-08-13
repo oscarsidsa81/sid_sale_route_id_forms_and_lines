d5391f38afe0f818d051bff4436306dba446c855
oscarsidsa81 <ovilches@sid-sa.com>
2025-08-13 09:41:03 +0200

    revisión condicionantes &amp; y &gt;

--- __manifest__.py
+++ __manifest__.py
@@ -6,7 +6,7 @@
     'summary': 'Campos en vista tree y form para visualizar las líneas de factura por línea en form y line',
     'description': 'Campos en vista tree y form para visualizar las líneas de factura por línea en form y line',
     'author': 'oscarsidsa81',
-    'depends': ['base','sale_management','account_accountant'],
+    'depends': ['base','sale_management','stock'],
     'data': [
         'views/sid_sale_route_id_forms_and_lines.xml'
     ],
