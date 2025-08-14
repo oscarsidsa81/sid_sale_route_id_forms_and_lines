ce084ada4161fa23cb51ad68ff207a7d736f2ef6
oscarsidsa81 <ovilches@sid-sa.com>
2025-08-14 14:10:25 +0200

    añadimos campo relacional para que podamos hacer las condiciones

--- __manifest__.py
+++ __manifest__.py
@@ -6,7 +6,7 @@
     'summary': 'Campos en vista tree y form para visualizar las líneas de factura por línea en form y line',
     'description': 'Campos en vista tree y form para visualizar las líneas de factura por línea en form y line',
     'author': 'oscarsidsa81',
-    'depends': ['base','sale_management','stock','oct_confrm_access_sale_order'],
+    'depends': ['base','sale_management','stock','oct_confrm_access_sale_order','oct_sale_order_by_line'],
     'data': [
         'views/sid_sale_route_id_forms_and_lines.xml'
     ],
